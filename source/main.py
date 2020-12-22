f2e33ce4bfa84fe40a3c0037dc6889b8b0e804f2
Petuuuhhh <pjswan8691@ung.edu>
2020-12-22 16:07:45 -0500

    fix to be playable

--- source/main.py
+++ source/main.py
@@ -12,88 +12,88 @@ class GenerateMonster():
         self.ac = ac
 
 def checkMissPlayer(defender):
-	"""
+    """
 
-	Returns a boolean token: if player missed or not.
+    Returns a boolean token: if player missed or not.
 
-	If defenders AC (Armor Class) is above players hit the token will evaluate to False,
-	and the player will respectively miss.
+    If defenders AC (Armor Class) is above players hit the token will evaluate to False,
+    and the player will respectively miss.
 
-	"""
+    """
 
-	global currPlayerHitToken
-	missChance = random.randrange(0, 25)
+    global currPlayerHitToken
+    missChance = random.randrange(0, 25)
 
-	if missChance <= defender:
-		currPlayerHitToken = False
-		return currPlayerHitToken
-	else:
-		currPlayerHitToken = True
-		return currPlayerHitToken
+    if missChance <= defender:
+        currPlayerHitToken = False
+        return currPlayerHitToken
+    else:
+        currPlayerHitToken = True
+        return currPlayerHitToken
 
 def checkMissOpponent(defender):
-	"""
+    """
 
-	Returns a boolean token: if opponent missed or not.
+    Returns a boolean token: if opponent missed or not.
 
-	If defenders AC (Armor Class) is above opponents hit, the token will evaluate to False,
-	and the opponent will respectively miss.
+    If defenders AC (Armor Class) is above opponents hit, the token will evaluate to False,
+    and the opponent will respectively miss.
 
-	"""
+    """
 
-	global currOpponentHitToken
-	missChance = random.randrange(0, 25) # make this variable
+    global currOpponentHitToken
+    missChance = random.randrange(0, 25) # make this variable
 
-	if missChance <= defender:
-		currOpponentHitToken = False
-		return currOpponentHitToken
-	else:
-		currPlayerHitToken = True
-		return currOpponentHitToken
+    if missChance <= defender:
+        currOpponentHitToken = False
+        return currOpponentHitToken
+    else:
+        currPlayerHitToken = True
+        return currOpponentHitToken
 
 def determineDamage(weapon, modifier, directed):
-	"""
+    """
 
-	Returns an integer: damage inflicted by the weapon.
+    Returns an integer: damage inflicted by the weapon.
 
-	Relative to the player/opponent's weapon, inflictDamage is called and the function's
-	effects to the opposing's HP is calculated.
+    Relative to the player/opponent's weapon, inflictDamage is called and the function's
+    effects to the opposing's HP is calculated.
 
-	"""
+    """
 
-	if weapon == "fists" or weapon == "claws":
-		return inflictDamage(player, 2 * modifier, 6 * modifier)
-	elif weapon == "Iron Broadsword":
-		return inflictDamage(opponent, 100, 250)
-	return
+    if weapon == "fists" or weapon == "claws":
+        return inflictDamage(player, 2 * modifier, 6 * modifier)
+    elif weapon == "Iron Broadsword":
+        return inflictDamage(opponent, 100, 250)
+    return
 
 def inflictDamage(inflicted, min, max):
-	"""
+    """
 
-	Returns damage inflicted to determineDamage: which is called in main().
+    Returns damage inflicted to determineDamage: which is called in main().
 
-	"""
-	damageInflicted = random.randrange(min, max+1)
-	if damageInflicted == 0:
-		return 'Miss!'
-	else:
-		inflicted.health-=damageInflicted
-		return damageInflicted
+    """
+    damageInflicted = random.randrange(min, max+1)
+    if damageInflicted == 0:
+        return 'Miss!'
+    else:
+        inflicted.health-=damageInflicted
+        return damageInflicted
 
 def getWinner(player, enemy):
-	"""
+    """
 
-	Returns winner of the match by comparing object's HP attribute once knocked below zero.
+    Returns winner of the match by comparing object's HP attribute once knocked below zero.
 
-	"""
+    """
 
-	if player.health > enemy.health:
-		print player.name, 'wins!'
-	else:
-		print enemy.name, 'wins!'
+    if player.health > enemy.health:
+        print(player.name, 'wins!')
+    else:
+        print(enemy.name, 'wins!')
 
 def getHP(character):
-	return character.health
+    return character.health
 
 
 
@@ -103,30 +103,30 @@ player = GenerateMonster('Paladin', 150, 200, 'Iron Broadsword', 15)
 
 def main():
 
-	playerInitialHealth = player.health
-	opponentInitialHealth = opponent.health
+    playerInitialHealth = player.health
+    opponentInitialHealth = opponent.health
 
-	while (opponent.health >= 0) and (player.health >= 0):
+    while (opponent.health >= 0) and (player.health >= 0):
 
-		time.sleep(1)
+        time.sleep(1)
 
-		if (currPlayerHitToken):
-			print "%s HP:" % player.name, getHP(player)
-			print "Damage to %s:" % opponent.name, determineDamage(player.weapon, 1, opponent.health)
-		else:
-			print '%s HP:' % player.name, getHP(player)
-	   		print '%s missed!' % player.name
+        if (currPlayerHitToken):
+            print("%s HP:" % player.name, getHP(player))
+            print("Damage to %s:" % opponent.name, determineDamage(player.weapon, 1, opponent.health))
+        else:
+            print('%s HP:' % player.name, getHP(player))
+            print('%s missed!' % player.name)
 
-		time.sleep(1)
+        time.sleep(1)
 
-	   	if(currOpponentHitToken):
-	   		print "%s HP:" % opponent.name, getHP(opponent)
-	   		print "Damage to: %s" % player.name, determineDamage(opponent.weapon, 1, player.health)
-	   	else:
-	   		print "%s HP:" % opponent.name, getHP(opponent)
-	   		print '%s missed!' % opponent.name
+        if(currOpponentHitToken):
+            print("%s HP:" % opponent.name, getHP(opponent))
+            print("Damage to: %s" % player.name, determineDamage(opponent.weapon, 1, player.health))
+        else:
+            print("%s HP:" % opponent.name, getHP(opponent))
+            print('%s missed!' % opponent.name)
 
-	getWinner(player, opponent)
+    getWinner(player, opponent)
 
 if __name__ == "__main__":
-	main()
+    main()
